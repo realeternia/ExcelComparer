c3fa970e255786c11d6b23e232210ec751a4949e
realeternia <narlonzjh@163.com>
2019-09-17 11:23:02 +0800

    use mutithread

--- ExcelComparer/ExcelComparer/Excel2Csv.cs
+++ ExcelComparer/ExcelComparer/Excel2Csv.cs
@@ -7,7 +7,7 @@ namespace ExcelComparer
 {
     public class Excel2Csv
     {
-        public static string Convert(string fileName)
+        public static string Convert(string fileName, bool isOld)
         {
             var fi = new FileInfo(fileName);
             var tempFile = Path.GetTempFileName();
@@ -18,7 +18,7 @@ namespace ExcelComparer
 
                 for (int i = 1; i <= workbook.Worksheets.Count; i++)
                 {
-                    WriteSheet(workbook.Worksheets[i], sw);
+                    WriteSheet(workbook.Worksheets[i], sw, isOld, i, workbook.Worksheets.Count);
                 }
             }
 
@@ -26,19 +26,45 @@ namespace ExcelComparer
             return tempFile;
         }
 
-        private static void WriteSheet(ExcelWorksheet sheetIn, StreamWriter sw)
+        private static void WriteSheet(ExcelWorksheet sheetIn, StreamWriter sw, bool isOld, int now, int total)
         {
             if (sheetIn.Name.StartsWith("~") || sheetIn.Dimension == null)
                 return;
 
+            int colCount = sheetIn.Dimension.End.Column;
+            for (int col = 2; col <= colCount; col++)
+            {
+                var dt = sheetIn.GetValue(9, col);
+                if (dt == null)
+                {
+                    colCount = col;
+                    break;
+                }
+            }
+
             for (int row = 1; row <= sheetIn.Dimension.End.Row; row++)
             {
-                for (int col = 1; col <= sheetIn.Dimension.End.Column; col++)
+                if (row > 14 && sheetIn.GetValue(row, 2) == null)
+                {
+                    break;
+                }
+
+                for (int col = 1; col <= colCount; col++)
                 {
                     sw.Write(sheetIn.GetValue(row, col) + "\t"); 
                 }
 
                 sw.WriteLine();
+
+                var progress = ((float) (now-1) / total) + (float)row / sheetIn.Dimension.End.Row / total;
+                if (isOld)
+                {
+                    Program.oldProgress = progress;
+                }
+                else
+                {
+                    Program.newProgress = progress;
+                }
             }
         }
 
--- ExcelComparer/ExcelComparer/Program.cs
+++ ExcelComparer/ExcelComparer/Program.cs
@@ -1,21 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace ExcelComparer
 {
     class Program
     {
+        private static string csvOldFile = "";
+        private static string csvNewFile = "";
+
+        public static float oldProgress;
+        public static float newProgress;
+
         static void Main(string[] args)
         {
             var tortoiseMergePos = args[0];
             var oldPath = args[1];
             var newPath = args[2];
 
-            var csvOldFile = Excel2Csv.Convert(oldPath);
-            var csvNewFile = Excel2Csv.Convert(newPath);
+            ThreadStart method = () => TaskRun(oldPath, true);
+            Thread t1 = new Thread(method);
+            t1.IsBackground = true;
+            t1.Start();
+
+            ThreadStart method2 = () => TaskRun(newPath, false);
+            Thread t2 = new Thread(method2);
+            t2.IsBackground = true;
+            t2.Start();
+
+            Console.WriteLine("开始解析文件");
+            while (csvOldFile == "" || csvNewFile == "")
+            {
+                Console.Write("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b对比进度{0:00.00}%", oldProgress * 50 + newProgress * 50);
+                Thread.Sleep(10);
+            }
+
+            t1.Join();
+            t2.Join();
 
             System.Diagnostics.Process.Start(tortoiseMergePos, string.Format("{0} {1}", csvOldFile, csvNewFile));
+        }
+
+        static void TaskRun(string path, bool isOld)
+        {
+            var name = Excel2Csv.Convert(path, isOld);
+            if (isOld)
+            {
+                csvOldFile = name;
+            }
+            else
+            {
+                csvNewFile = name;
+            }
         }
     }
 }
